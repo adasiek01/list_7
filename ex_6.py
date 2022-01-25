78b92f54423e5cef43422e493ef97273ad748015
Adam Kawałko <262329@student.pwr.edu.pl>
2022-01-25 23:07:29 +0100

    sposób napisany w komentarzu

---
+++ ex_6.py
@@ -0,0 +1,54 @@
+"""
+K-Kanibal
+M-Misjonarz
+
+Lewy brzeg rzeki  Łódź Prawy brzeg rzeki
+
+3K
+3M
+
+2K      ->         1K        0K
+2M                 1M        0M
+
+2K      <-         0K        1K
+2M                 1M        0M
+
+0K      ->         2K        1K
+3M                 0M        0M
+
+0K      <-         1K        2K
+3M                 0M        0M
+
+1K      ->         0K        2K
+1M                 2M        0M
+
+1K      <-         1K        1K
+1M                 1M        1M
+
+2K      ->         0K        1K
+0M                 2M        1M
+
+2K      <-         1K        0K
+0M                 0M        3M
+
+1K      ->         2K        0K
+0M                 0M        3M
+
+1K      <-         1K        1K
+0M                 0M        3M
+
+0K      ->         2K        1K
+0M                 0M        3M
+
+0K                 0K        3K
+0M                 0M        3M
+"""
+
+
+
+
+
+
+
+
+
