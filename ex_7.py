6b0832db9579722c2a38c6256bddaf2bcb4539ec
adasiek01 <81614226+adasiek01@users.noreply.github.com>
2022-01-31 23:47:03 +0100

    Delete ex_7.py

--- ex_7.py
+++
@@ -1,6 +0,0 @@
-"""
-Mamy 2 kanistry:3l(A) i 4l(B). Napełniamy cały kanister B i przelewamy wodę do kanistra A,
- tak żeby się nic nie wylało. W kanistrze B zostanie 1l wody. Następnie opróżniamy kanister A i wlewamy do niego 1l
- wody z kanistra B i napełniamy kanister B. Później przelewamy z niego wodę do kanistra A, tak żeby nic się nie wylało.
- W kanistrze B zostaną nam 2l wody!
- """
